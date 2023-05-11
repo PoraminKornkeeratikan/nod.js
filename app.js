ed6beab40c4b9da291832a0b58703989c814dc16
Poramin kornkeeratikan <Poramin.Kornkeeratikan@gmail.com>
2023-05-11 12:10:58 +0700

    add all file of project
    hello aodzi

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const chalk = require('chalk')
+const { listen } = require('express/lib/application');
+const app = express();
+const port = 3000;
+
+app.get("/", (req,res) => {
+
+    res.send('Hello my name is poramin');
+})
+
+app.listen(port, ()=>{
+
+    console.log("Listening on port" + chalk.green(" : " + port));
+})
